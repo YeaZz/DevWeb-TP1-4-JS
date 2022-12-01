fca9bfb9adecd3a9560df7c4d846c00ee5478a22
Noa Cavalcante <cavalcantenoa@gmail.com>
2022-12-01 20:30:44 +0100

    refactoring + cancel submit if not correct

--- assets/js/subscribe.js
+++ assets/js/subscribe.js
@@ -1,5 +1,7 @@
-let inputs = document.getElementsByClassName("form")
-let errors = document.getElementsByClassName("error")
+const form = document.getElementById("form") 
+
+const inputs = document.getElementsByClassName("form")
+const errors = document.getElementsByClassName("error")
 
 const postalRegex = new RegExp(/[0-9]{5}/)
 const emailRegex = new RegExp(/[a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{1,6}/)
@@ -7,20 +9,32 @@ const emailRegex = new RegExp(/[a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{1,6}/)
 for (let i = 0; i < inputs.length; i++) {
     let input = inputs[i]
     let error = errors[i]
-    if (input.value == "")
-        error.innerHTML = "* Champ obligatoire"
+    error.innerHTML = verifyInput(input)
     input.addEventListener("keyup", event => {
-        let msg = ""
-        if (input.value == "") {
-            msg = "* Champ obligatoire"
-        } else if (input.id == "email") {
-            if (!emailRegex.test(input.value))
-                msg = "* Format incorrect"
-        } else if (input.id == "postal") {
-            if (!postalRegex.test(input.value))
-                msg = "* Format incorrect"
-        }
-        error.innerHTML = msg
+        error.innerHTML = verifyInput(input)
     })
 }
 
+form.addEventListener("submit", event => {
+    let canSubmit = true
+    for (let i = 0; i < inputs.length; i++) {
+        if (verifyInput(inputs[i]) != "")
+            canSubmit = false
+    }
+    if (!canSubmit)
+        event.preventDefault()
+})
+
+function verifyInput(input) {
+    let msg = ""
+    if (input.value == "") {
+        msg = "* Champ obligatoire"
+    } else if (input.id == "email") {
+        if (!emailRegex.test(input.value))
+            msg = "* Format incorrect"
+    } else if (input.id == "postal") {
+        if (!postalRegex.test(input.value))
+            msg = "* Format incorrect"
+    }
+    return msg
+}
