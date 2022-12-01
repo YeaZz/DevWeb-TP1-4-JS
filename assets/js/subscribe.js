4e2e1a1bb87598749439a4fde70b5f02385be501
Noa Cavalcante <cavalcantenoa@gmail.com>
2022-12-01 10:16:12 +0100

    add email regex

--- assets/js/subscribe.js
+++ assets/js/subscribe.js
@@ -1,7 +1,8 @@
 let inputs = document.getElementsByClassName("form")
 let errors = document.getElementsByClassName("error")
 
-const postalRegex = new RegExp(/[0-9]{5}/, 'g');
+const postalRegex = new RegExp(/[0-9]{5}/);
+const emailRegex = new RegExp(/[a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{1,6}/)
 
 for (let i = 0; i < inputs.length; i++) {
     if (inputs[i].value == "") {
@@ -12,6 +13,9 @@ for (let i = 0; i < inputs.length; i++) {
         let msg = "";
         if (input.value == "") {
             msg = "* Champ obligatoire"
+        } else if (input.id == "email") {
+            if (!emailRegex.test(input.value))
+                msg = "* Format incorrect"
         } else if (input.id == "postal") {
             if (!postalRegex.test(input.value))
                 msg = "* Format incorrect"
